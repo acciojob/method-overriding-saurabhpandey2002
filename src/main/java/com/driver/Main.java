e4eb779dd81669dbec63821848465019db7a9186
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-02-12 15:01:54 +0530

    add4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 public class Main {
         public static void main(String[] args){
             B obj = new B();
-            obj.meth();
+            System.out.println(obj.meth());
         }
         static class A{
             static String meth(){
