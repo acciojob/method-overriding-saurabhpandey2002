b70dd3d1b6a8deabdd23cc2d9ade3979e18dea4a
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-02-12 14:18:34 +0530

    Some Changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
-
+class A{
+    void run(){
+        System.out.println("Invoking method from class A");
+    }
+}
+class B extends A{
+    void run(){
+        System.out.println("Method is overridden in Extendend class B");
+    }
+}
 public class Main {
-  
+        public static void main(String[] args){
+            B obj = new B();
+            obj.run();
+        }
+
 }
