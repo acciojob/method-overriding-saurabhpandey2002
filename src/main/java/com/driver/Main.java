7f6d1c392e34725b872b67c5e202bbf004c51287
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-02-13 07:34:04 +0530

    add8

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,18 +8,14 @@ public class Main {
     }
 
     public static class B extends A {
-        @Override
         public String meth() {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args) {
-        // Task 3: create object of class B and call method meth of class A with it
-        B objB = new B();
-        System.out.println(objB.meth()); // Output: Method is overridden in Extended class B
-
-        // Task 5: call this overridden method from obj of class B
-        B objB1 = new B();
-       System.out.println(objB1.meth()); // Output: Method is overridden in Extended class B
+        B b = new B();
+        System.out.println(b.meth());
+        B b1 = new B();
+        System.out.println(b1.meth());
     }
 }
